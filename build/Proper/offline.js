cdb5e79ed63ba89990769e48437113ac3c9fee46
thomsonj <thomsonj@chesterfield.ac.uk>
2018-08-27 23:14:20 +0100

    Add files via upload

---
+++ build/Proper/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1535406734,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/huntback-sheet0.png",
+		"images/location-sheet0.png",
+		"images/location-sheet1.png",
+		"images/gobutton-sheet0.png",
+		"images/endbackground1-sheet0.png",
+		"images/endback-sheet0.png",
+		"images/posterclip-sheet0.png",
+		"images/posterclip-sheet1.png",
+		"images/numpad-sheet0.png",
+		"images/k0-sheet0.png",
+		"images/k1-sheet0.png",
+		"media/captain pugwash.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
